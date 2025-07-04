8143423a2bdf2daba1d0066baa969d0bdc35f4bc
Cosmln23 <scinterim09@gmail.com>
2025-07-05 00:13:11 +0200

    cc

--- server/rag/index/parser/file_loader/csv_loader.py
+++ server/rag/index/parser/file_loader/csv_loader.py
@@ -1,44 +1,37 @@
 import os
-from llama_parse import LlamaParse
 import pandas as pd
-from server.logger.logger_config import my_logger as logger
 
-USE_LLAMA_PARSE = int(os.getenv('USE_LLAMA_PARSE'))
+from server.logger.logger_config import my_logger as logger
+from server.rag.index.parser.file_loader.fitz_loader import PyMuPdfLoader
+
+USE_LLAMA_PARSE = int(os.getenv('USE_LLAMA_PARSE', 0))
 LLAMA_CLOUD_API_KEY = os.getenv('LLAMA_CLOUD_API_KEY')
 
 
 class AsyncCsvLoader:
     def __init__(self, file_path: str) -> None:
-        logger.info(f"[FILE LOADER] init csv, file_path: '{file_path}'")
+        logger.info(f"[FILE LOADER] init csv/pdf, file_path: '{file_path}'")
         self.file_path = file_path
 
     async def get_content(self) -> str:
         try:
             content = ''
 
-            if USE_LLAMA_PARSE:
-                parser = LlamaParse(
-                    api_key=LLAMA_CLOUD_API_KEY,
-                    result_type="markdown",
-                )
+            # Dacă este setat USE_LLAMA_PARSE, folosim loaderul local PyMuPdfLoader pentru PDF-uri
+            if USE_LLAMA_PARSE and self.file_path.lower().endswith('.pdf'):
+                loader = PyMuPdfLoader(self.file_path)
+                content = loader.load()
 
-                text_vec = []
-
-                import nest_asyncio
-                nest_asyncio.apply()
-
-                documents = parser.load_data(self.file_path)
-                for doc in documents:
-                    text_vec.append(doc.text)
-                content = "\n\n".join(text_vec)
-            else:
-                # Load the CSV file into a DataFrame
+            # Pentru CSV-uri normale
+            elif self.file_path.lower().endswith('.csv'):
                 df = pd.read_csv(self.file_path)
-                # Convert the DataFrame to a Markdown string
                 content = df.to_markdown(index=False)
 
-                if not content:
-                    logger.warning(f"file_path: '{self.file_path}' is empty!")
+            else:
+                logger.warning(f"Unsupported file type: {self.file_path}")
+
+            if not content:
+                logger.warning(f"file_path: '{self.file_path}' is empty!")
             return content
         except Exception as e:
             logger.error(f"get_content is failed, exception: {e}")
